cc8adfd658020be45bcea8c23c20ffa5059de7d6
sumayya <sumayyakhanam99@gmail.com>
2022-03-26 18:47:27 +0300

    first commit

--- useroperations4/settings.py
+++ useroperations4/settings.py
@@ -124,6 +124,8 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = BASE_DIR/"staticfiles"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
