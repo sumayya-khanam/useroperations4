43e7f1e88b2566e376fb50a76041f50c15c88ae6
sumayya <sumayyakhanam99@gmail.com>
2022-04-02 17:42:47 +0300

    my sixth commit

--- useroperations4/settings.py
+++ useroperations4/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ambn0yw)1ly77vlbl)&qcm9l(2y^^*fg@79%ohg9cla)u4g03j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
